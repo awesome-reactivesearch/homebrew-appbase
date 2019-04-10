ac918be3497d81282a570e2a4c2561b209673350
Jeet Parekh <12874561+jeet-parekh@users.noreply.github.com>
2019-04-10 22:00:16 +0530

    update formula to abc-0.10.2

--- abc.rb
+++ abc.rb
@@ -1,12 +1,12 @@
 class Abc < Formula
   desc "Power of appbase.io via CLI, with nifty imports from your favorite data sources"
   homepage "https://github.com/appbaseio/abc"
-  url "https://github.com/appbaseio/abc/releases/download/0.8.0/abc-darwin-0.8.0.zip"
-  sha256 "5011f368c6111e565da5a46230fc5e70344e792029d06898be1b1a98f50c4761"
+  url "https://github.com/appbaseio/abc/releases/download/0.10.2/abc-darwin-0.10.2.zip"
+  sha256 "d4a554a2c689d427ede981707fc61f128227d497c2404c2831ea766f6a9ea403"
 
   def install
-    bin.install "abc-0.8.0"
-    mv "#{bin}/abc-0.8.0", "#{bin}/abc"
+    bin.install "abc-0.10.2"
+    mv "#{bin}/abc-0.10.2", "#{bin}/abc"
   end
 
   test do
